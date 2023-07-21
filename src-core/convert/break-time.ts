a02a7da76fa7ededbb9c9af5e624c48421ef37e9
Daniel Costa <costa.xdaniel@gmail.com>
2023-07-21 14:01:17 +0000

    hash

--- src-core/convert/break-time.ts
+++ src-core/convert/break-time.ts
@@ -1,27 +1,54 @@
 import Path from "path";
 import {decompose} from "../util/decompose";
+import fs from "fs";
+//language=file-reference
+let breakTimes = Path.join(__dirname, "../../source/assets/silent" );
+let maps = fs.readdirSync( breakTimes ).map(filename => {
+    let [ name, sTime, final ] = filename.split("-");
+    let [ unit, extension ] = (final||"").split(".");
+    let time = Number( sTime );
+    if( !time || Number.isNaN( time ) || !Number.isFinite( time ) ) time = null;
+
+    return {
+        name: filename,
+        time: time,
+        unit,
+        extension,
+        filename: Path.join(breakTimes, filename)
+
+    };
+}).filter( value => {
+    return !!value.time
+        && !!value.name
+        && !!value.unit
+        && !!value.extension
+        && !!value.filename
+});
+
+
+let _pieces = {};
+maps.forEach( value => {
+    _pieces[ value.time ] = value.filename
+})
+
 
 const breakTime = {
-    times:[ 5, 4, 3, 2, 1, 0.5, 0.1 ],
-    //language=file-reference
-    pieces:{
-        5: Path.join( __dirname, "../../source/break-times/5s.mp3" ),
-        4: Path.join( __dirname, "../../source/break-times/4s.mp3" ),
-        3: Path.join( __dirname, "../../source/break-times/3s.mp3" ),
-        2: Path.join( __dirname, "../../source/break-times/2s.mp3" ),
-        1: Path.join( __dirname, "../../source/break-times/1s.mp3" ),
-        0.5: Path.join( __dirname, "../../source/break-times/0.5s.mp3" ),
-        0.1: Path.join( __dirname, "../../source/break-times/0.1s.mp3" ),
-    }
+    times:maps.map( value => value.time ).sort( (a, b) => {
+        if( a > b ) return 1;
+        else if( b > a ) return  -1;
+        else return 0
+    }).reverse(),
+    pieces: _pieces
 }
 
 export function breakTimeAudios(time:number ):string[]{
-    // console.log( files )
     return  decompose(time, breakTime.times).map(value => {
         return breakTime.pieces[value];
     });
-    // return []
 }
+
+console.log( breakTimeAudios(13.4));
+
 
 export function timePause( charLength:number){
     let times =  4+( ( charLength * 75 / 1000 )/ 3 );
--- test/test2.js
+++ test/test2.js
@@ -1,3 +1,23 @@
-let decompose = ( value, pieces )=>{
+const {audioJoin} = require("../src-core/util/file-join");
+const Path = require("path");
+const fs = require("fs");
+audioJoin( {
+    //language=file-reference
+    files:[
+        Path.join( __dirname, "../source/break-times/silent-0.1-s.mp3" ),
+        Path.join( __dirname, "../source/break-times/silent-5.0-s.mp3" ),
+        Path.join( __dirname, "../source/break-times/silent-5.0-s.mp3" ),
+        Path.join( __dirname, "../source/break-times/silent-5.0-s.mp3" ),
+        Path.join( __dirname, "../source/break-times/silent-5.0-s.mp3" )
+    ],
+    concatFile: Path.join( __dirname, "file.mp3")
+}).then( value => {
+    console.log( value );
 
-}
+    //language=file-reference
+    console.log(fs.readFileSync(Path.join(__dirname, "../source/break-times/silent-1.0-s.mp3")).toString())
+    //language=file-reference
+    console.log(fs.readFileSync(Path.join(__dirname, "../source/assets/silent/breakTime-1-s.mp3")).toString())
+});
+
+
